c09a53c0944635cb066913afbff0789c182d0932
Lydia <lydia.chatziioannou@gmail.com>
2019-04-09 19:58:19 +0100

    update

--- Website/welcome.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-session_start();                                // once directed here from validate page we need to start a session here too - access field from validate page (from line 15)
-echo "Welcome". $_SESSION['email_address'];
-echo "<a href='logout.php'>logout</a>";         //  send or link to the next page - could be myRentals for our DB (that will also need a session i think) 
-
